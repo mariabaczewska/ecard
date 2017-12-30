ca5f26a279ab361382f65e3d7c792e05d16ae71c
DESKTOP-M63HLU2\Maria <m.baczewska94@gmail.com>
2017-12-30 18:47:42 +0100

    My ecard app, first commit

---
+++ aplikacja/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Karta
+
+admin.site.register(Karta)
+# Register your models here.
---
+++ aplikacja/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Karta',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('name', models.CharField(max_length=200)),
+                ('surname', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
